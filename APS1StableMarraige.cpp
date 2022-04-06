c56583deba5186215abc849d582f9cdfaa49ea79
Gautam-git1050 <91388821+Gautam-git1050@users.noreply.github.com>
2022-04-06 23:56:05 +0530

    Update APS1StableMarraige.cpp

--- APS1StableMarraige.cpp
+++ APS1StableMarraige.cpp
@@ -1,75 +1,113 @@
-/**
- * @ Author: Aditya Kumar Pandey
- * @ Create Time: 2020-03-27 22:37:01
- * @ Description:This is Solution for Stable Marriage Problem which is solved by Gayle Shrpley Algorithm Which is running at effeciency of O(n^2)
- * @ Link:https://www.geeksforgeeks.org/stable-marriage-problem/
- *        For 2D array as argument https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function
- */
-
-#include <bits/stdc++.h>
+// C++ program for stable marriage problem
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
 using namespace std;
 
-bool  isBetter(int m , int w , int m1 , int Women [4][4] , int n){ // Here m1 is the man the woman is already engaged to true is m is better than m1
-    for (int i =0 ; i < n ; i++){
-        if(Women[w][i] == m)
-            return true;
-        if(Women[w][i] == m1)
-            return false;
-    } 
+// Number of Men or Women
+#define N 4
+
+// This function returns true if woman 'w' prefers man 'm1' over man 'm'
+bool wPrefersM1OverM(int prefer[2*N][N], int w, int m, int m1)
+{
+	// Check if w prefers m over her current engagement m1
+	for (int i = 0; i < N; i++)
+	{
+		// If m1 comes before m in list of w, then w prefers her
+		// current engagement, don't do anything
+		if (prefer[w][i] == m1)
+			return true;
+
+		// If m comes before m1 in w's list, then free her current
+		// engagement and engage her with m
+		if (prefer[w][i] == m)
+		return false;
+	}
 }
 
-void StableMarriage(int n , int Men[4][4] ,int Women [4][4] ){
-    int *wPartner; // This array will store the current partner of the women at ith index if ith index has -1 then women is free
-    wPartner = new int[n];
-    bool *mPaired;  // This array will store the status of men if the ith index of this array is false then man is free and if ith index is True then man is engaged
-    mPaired = new bool[n];
-    memset(wPartner,-1,sizeof(wPartner)*sizeof(int));
-    memset(mPaired,false,sizeof(mPaired)*sizeof(bool));
-    int freeM =n;
-    while (freeM>0){
-        int m; // m will be men index
-        for (int i = 0 ; i<n ; i++){
-            if (mPaired[i] == false)
-                {m = i;
-                // cout<<"Man = "<<m;
-                break;
-                }
-        }
-        // cout<<"HERE";
-        for (int i = 0 ; i<n && (mPaired[m] == false) ;i++){
-            int w; // w will be current women index
-            w = Men[m][i];
-            if (wPartner[w] == -1){
-                wPartner[w] = m;
-                mPaired[m] = true;
-                freeM--;
-                cout<<"Women"<<w<<"PAired with Man"<<m<<"\n";
-            }
-            else if(isBetter(m,w,wPartner[w],Women,n)){
-                mPaired[wPartner[w]]=false;
-                mPaired[m]=true;
-                wPartner[w]=m;
-                cout<<"Women"<<w<<"PAired with Man"<<m<<"\n";
-            }
-        }
-    }
-    cout<<"Women\t---x---Men\n";
-    for(int i = 0 ; i<n ; i++){
-        cout<<i<<"\t---x---"<<wPartner[i]<<"\n";
-    }
+// Prints stable matching for N boys and N girls. Boys are numbered as 0 to
+// N-1. Girls are numbered as N to 2N-1.
+void stableMarriage(int prefer[2*N][N])
+{
+	// Stores partner of women. This is our output array that
+	// stores passing information. The value of wPartner[i]
+	// indicates the partner assigned to woman N+i. Note that
+	// the woman numbers between N and 2*N-1. The value -1
+	// indicates that (N+i)'th woman is free
+	int wPartner[N];
+
+	// An array to store availability of men. If mFree[i] is
+	// false, then man 'i' is free, otherwise engaged.
+	bool mFree[N];
+
+	// Initialize all men and women as free
+	memset(wPartner, -1, sizeof(wPartner));
+	memset(mFree, false, sizeof(mFree));
+	int freeCount = N;
+
+	// While there are free men
+	while (freeCount > 0)
+	{
+		// Pick the first free man (we could pick any)
+		int m;
+		for (m = 0; m < N; m++)
+			if (mFree[m] == false)
+				break;
+
+		// One by one go to all women according to m's preferences.
+		// Here m is the picked free man
+		for (int i = 0; i < N && mFree[m] == false; i++)
+		{
+			int w = prefer[m][i];
+
+			// The woman of preference is free, w and m become
+			// partners (Note that the partnership maybe changed
+			// later). So we can say they are engaged not married
+			if (wPartner[w-N] == -1)
+			{
+				wPartner[w-N] = m;
+				mFree[m] = true;
+				freeCount--;
+			}
+
+			else // If w is not free
+			{
+				// Find current engagement of w
+				int m1 = wPartner[w-N];
+
+				// If w prefers m over her current engagement m1,
+				// then break the engagement between w and m1 and
+				// engage m with w.
+				if (wPrefersM1OverM(prefer, w, m, m1) == false)
+				{
+					wPartner[w-N] = m;
+					mFree[m] = true;
+					mFree[m1] = false;
+				}
+			} // End of Else
+		} // End of the for loop that goes to all women in m's list
+	} // End of main while loop
+
+
+	// Print the solution
+	cout << "Woman Man" << endl;
+	for (int i = 0; i < N; i++)
+	cout << " " << i+N << "\t" << wPartner[i] << endl;
 }
 
+// Driver program to test above functions
+int main()
+{
+	int prefer[2*N][N] = { {7, 5, 6, 4},
+		{5, 4, 6, 7},
+		{4, 5, 6, 7},
+		{4, 5, 6, 7},
+		{0, 1, 2, 3},
+		{0, 1, 2, 3},
+		{0, 1, 2, 3},
+		{0, 1, 2, 3},
+	};
+	stableMarriage(prefer);
 
-
-int main(){
-    int Men[4][4]={{3,1,2,0},
-                   {1,0,2,3},
-                   {0,1,2,3},
-                   {0,1,2,3}};
-    int Women[4][4]={   {0,1,2,3},
-                        {0,1,2,3},
-                        {0,1,2,3},
-                        {0,1,2,3}};
-    StableMarriage(4,Men,Women);
-    return 0;
+	return 0;
 }
