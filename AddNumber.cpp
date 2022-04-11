c66594576e2431a72801af19040ca1eb31a00385
Gautam-git1050 <91388821+Gautam-git1050@users.noreply.github.com>
2022-04-11 13:23:28 +0530

    Update AddNumber.cpp

--- AddNumber.cpp
+++ AddNumber.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int add(int a, int b)
 {
-for(int i=1;i<=b;i++) //for loop will start from 1 and move till the value of second
-						// number , first number(a) is incremented in for loop
+for(int i=1;i<=b;i++) 						
 { a++;			
 }
 return a;
@@ -12,10 +11,10 @@ return a;
 
 int main(){
 
-int a=add(10,32); // first number is 10 and second number is 32 , for loop will start
-cout<<a;	 // from 1 and move till 32 and the value of a is incremented 32 times
-			// which will give us the total sum of two numbers
+int a=add(10,32); 
+cout<<a;	 
+			
 return 0;	
 
 }
-// This code is contributed by sayamsobti
+
